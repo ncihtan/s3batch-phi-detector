32472d5aa569ca8c1f0d0770544590b8e432a7db
Adam Taylor <adam.taylor@sagebase.org>
2021-11-02 11:22:40 +0000

    continue if chunk == 0

--- scripts/aws_detect_pii.py
+++ scripts/aws_detect_pii.py
@@ -213,7 +213,7 @@ def detect_pii(session, data: str, chunk_size: int = 4096) -> List[str]:
 
     results = []
     for i, chunk in enumerate(split_data):
-        if len(chunk) > 0:
+        if len(chunk) == 0:
             continue
         pii_results = comprehend.detect_pii_entities(Text=chunk, LanguageCode='en')
         entities = pii_results.get('Entities', None)
